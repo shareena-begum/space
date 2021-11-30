1c80810e9a6d7a26e848efa5476b269f46362d2d
shareena <shareena.fsp@gmail.com>
2021-11-30 15:32:44 +0100

    map function

---
+++ mapfunction.js
@@ -0,0 +1,54 @@
+/* 
+.map() method:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+*/
+
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+const squared = nums.map(function(num){
+   return num * num
+})
+
+console.log(squared) 
+// ---->
+
+[1, 4, 9, 16, 25]
+
+
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+const uppercase = names.map((name) => {
+    return name[0].toUpperCase() + name.slice(1)
+})
+console.log(uppercase)
+
+
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>, <p>Charmander</p>, <p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>, <p>Charmander</p>, <p>Squirtle</p>"]
+// Your code here
+const elements = pokemon.map(mon => {
+    return `<p>${mon}</p>`
+})
+console.log(elements)
